5fee8de7aafdef704e7820be186b7607128394d2
Matteo Trapani <matteo.trapani@ennova-research.com>
2018-03-24 18:19:24 +0100

    fix

--- server/server.js
+++ server/server.js
@@ -18,7 +18,6 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
-    console.log('roooooooooooooooms', users.getRooms());
     socket.broadcast.emit('updateRoomsList', users.getRooms());
 
     socket.on('join', (data, callback) => {
